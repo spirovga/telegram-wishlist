50452df7a6e7c16a5b804f7ddba7f24c11345e58
gregory <grigory.spirov@indels.org>
2025-02-28 16:13:57 +0500

    Deploy to GitHub Pages and add .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/telegram-wishlist/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
